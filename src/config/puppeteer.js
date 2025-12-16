516a09bcbcf2f759c7cdbbb4ebfac541fe58b6fa
zee <mohammadzidane058@gmail.com>
2025-12-16 09:36:09 +0700

    update: puppeteer config

--- src/config/puppeteer.js
+++ src/config/puppeteer.js
@@ -1,6 +1,12 @@
+import puppeteer from "puppeteer-core";
+
 export const PUPPETEER_CONFIG = {
-  executablePath: "/usr/bin/chromium",
-  headless: "new",
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  defaultViewport: null
+  executablePath: "/usr/bin/chromium-browser",
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu"
+  ]
 };

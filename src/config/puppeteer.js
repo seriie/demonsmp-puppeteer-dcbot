3234da049b6318c3e09f2e2f8cfccc9834d7c6e8
zee <mohammadzidane058@gmail.com>
2025-12-16 07:09:14 +0700

    update: puppeteer head

--- src/config/puppeteer.js
+++ src/config/puppeteer.js
@@ -1,11 +1,5 @@
 export const PUPPETEER_CONFIG = {
-  headless: false,
-  slowMo: 50,
-  defaultViewport: null,
-  args: [
-    "--no-sandbox",
-    "--disable-setuid-sandbox",
-    "--disable-dev-shm-usage",
-    "--start-maximized"
-  ]
-}
+  executablePath: "/usr/bin/chromium",
+  headless: "new",
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+};

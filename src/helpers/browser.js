be10a45534225a31bde91865b40ca9cbf6d29eef
zee <mohammadzidane058@gmail.com>
2025-12-16 07:40:25 +0700

    update: puppeteer config

--- src/helpers/browser.js
+++ src/helpers/browser.js
@@ -1,19 +1,16 @@
-import puppeteer from "puppeteer"
-import { PUPPETEER_CONFIG } from "../config/puppeteer.js"
+import puppeteer from "puppeteer";
+import { PUPPETEER_CONFIG } from "../config/puppeteer.js";
 
-let browser
-let page
+let browser;
+let page;
 
 export async function getPage() {
   if (!browser) {
-    browser = await puppeteer.launch({
-      ...PUPPETEER_CONFIG,
-    })
-
-    page = await browser.newPage()
+    browser = await puppeteer.launch(PUPPETEER_CONFIG);
+    page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
-    )
+    );
   }
-  return page
+  return page;
 }

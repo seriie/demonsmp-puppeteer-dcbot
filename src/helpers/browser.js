9278a65a489049bda41c982a8a4a3dd1f2070ec5
zee <mohammadzidane058@gmail.com>
2025-12-16 09:54:43 +0700

    update: puppeteer

--- src/helpers/browser.js
+++ src/helpers/browser.js
@@ -1,23 +1,25 @@
 import puppeteer from "puppeteer";
-import { PUPPETEER_CONFIG } from "../config/puppeteer.js";
-import fs from "fs";
-
-console.log("Puppeteer executable:", puppeteer.executablePath());
-
-console.log("chromium:", fs.existsSync("/usr/bin/chromium"));
-console.log("chromium-browser:", fs.existsSync("/usr/bin/chromium-browser"));
-console.log("lib chromium:", fs.existsSync("/usr/lib/chromium/chromium"));
 
 let browser;
 let page;
 
 export async function getPage() {
   if (!browser) {
-    browser = await puppeteer.launch({...PUPPETEER_CONFIG});
+    browser = await puppeteer.launch({
+      headless: "new",
+      executablePath: puppeteer.executablePath(), // 🔥 INI PENTING
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage"
+      ],
+    });
+
     page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
     );
   }
+
   return page;
 }

9d2786ee389888117020f5f23f854daa317b5065
zee <mohammadzidane058@gmail.com>
2025-12-17 11:18:03 +0700

    update: aternos login

--- src/helpers/aternos.login.js
+++ src/helpers/aternos.login.js
@@ -11,70 +11,80 @@ export async function loginAternos() {
   await page.goto("https://aternos.org/go", {
     waitUntil: "networkidle2",
   });
-  
+
   const usedCookie = await loadCookies(page);
   if (usedCookie) {
+    await page.goto("https://aternos.org/servers", {
+      waitUntil: "domcontentloaded",
+    });
+
+    try {
+      await page.waitForSelector(".server-body[data-id]", { timeout: 5000 });
+      console.log("🍪 Cookies loaded");
       console.log("✅ Login via cookie success");
+    } catch {
+      console.log("⚠️ Cookie invalid");
+    }
   }
-  
+
   if (!usedCookie) {
     console.log("⚠️ Cookie failed / login manually...");
-    
+
     await page.goto("https://aternos.org/go/", {
       waitUntil: "networkidle2",
     });
-    
+
     await page.waitForSelector(".go-input-group.join-right input", {
       visible: true,
     });
     await page.type(".go-input-group.join-right input", ENV.ATERNOS_USER);
-    
+
     await page.waitForSelector(".go-input-group.join-both input", {
       visible: true,
     });
-    
+
     await page.type(".go-input-group.join-both input", ENV.ATERNOS_PASS);
-    
+
     await Promise.all([
       page.click(".login-button"),
       page.waitForNavigation({ waitUntil: "networkidle2" }),
     ]);
-    
+
     console.log("⌛ Inputing manually...");
-    
+
     await page.waitForFunction(() => location.href.includes("/servers"), {
       timeout: 0,
     });
-    
+
     await saveCookies(page);
     console.log("✅ Login manual + cookie saved");
   }
-  
+
   console.log("🧠 Selecting server...");
-  
+
   await page.waitForSelector(".server-body[data-id]", {
     visible: true,
     timeout: 0,
   });
-  
+
   const serverId = await page.$eval(
     ".server-body[data-id]",
     (el) => el.dataset.id
   );
-  
+
   console.log("🎮 Server ID:", serverId);
-  
+
   const serverEl = await page.$(`.server-body[data-id="${serverId}"]`);
   await serverEl.click();
 
   console.log(`🖱️ Server with id: ${serverId} clicked`);
-  
-  await sleep(2000)
-  
-  await waitPageReady(page)
-  await skipAternosAds(page)
 
-  console.log("🔃 Waitiong for selector")
+  await sleep(2000);
+
+  await waitPageReady(page);
+  await skipAternosAds(page);
+
+  console.log("🔃 Waitiong for selector");
 
   await page.waitForSelector(".server-ip", {
     visible: true,

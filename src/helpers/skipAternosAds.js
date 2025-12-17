848213dbe5b2830c963755a656cbbfb7039f964a
zee <mohammadzidane058@gmail.com>
2025-12-17 11:27:39 +0700

    update: aternos login

--- src/helpers/skipAternosAds.js
+++ src/helpers/skipAternosAds.js
@@ -24,25 +24,25 @@ export async function skipAternosAds(page, options = {}) {
       return true
     }
 
-    let closeBtn = await page.$(closeSelector)
-    if (closeBtn) {
-      console.log("📢 Blocking ad detected (DOM) → closing")
-      await closeBtn.click()
-      await sleep(500)
-      return true
-    }
+    // let closeBtn = await page.$(closeSelector)
+    // if (closeBtn) {
+    //   console.log("📢 Blocking ad detected (DOM) → closing")
+    //   await closeBtn.click()
+    //   await sleep(500)
+    //   return true
+    // }
 
-    for (const frame of page.frames()) {
-      try {
-        const btn = await frame.$(closeSelector)
-        if (btn) {
-          console.log("📢 Blocking ad detected (iframe) → closing")
-          await btn.click()
-          await sleep(500)
-          return true
-        }
-      } catch {}
-    }
+    // for (const frame of page.frames()) {
+    //   try {
+    //     const btn = await frame.$(closeSelector)
+    //     if (btn) {
+    //       console.log("📢 Blocking ad detected (iframe) → closing")
+    //       await btn.click()
+    //       await sleep(500)
+    //       return true
+    //     }
+    //   } catch {}
+    // }
 
     await sleep(300)
   }

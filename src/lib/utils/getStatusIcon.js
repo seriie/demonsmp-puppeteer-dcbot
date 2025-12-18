910c91e79164593d94739a34de60169e5e8b4119
zee <mohammadzidane058@gmail.com>
2025-12-18 13:08:26 +0700

    feat: status icon

---
+++ src/lib/utils/getStatusIcon.js
@@ -0,0 +1,10 @@
+export function getStatusIcon(status = "") {
+  const s = status.toLowerCase();
+
+  if (s === "online") return "🟢";
+  if (s.includes("starting")) return "🔄";
+  if (s.includes("loading")) return "🔄";
+  if (s.includes("saving")) return "📥";
+
+  return "🔴";
+}
--- src/services/aternos.service.js
+++ src/services/aternos.service.js
@@ -1,32 +1,33 @@
-import { loginAternos } from "../helpers/aternos.login.js"
-import { startServer } from "../helpers/aternos.server.js"
-import { readServerState } from "../helpers/serverState.js"
-import { startAternosPoller } from "./aternos.poller.js"
+import { loginAternos } from "../helpers/aternos.login.js";
+import { startServer } from "../helpers/aternos.server.js";
+import { readServerState } from "../helpers/serverState.js";
+import { startAternosPoller } from "./aternos.poller.js";
+import { getStatusIcon } from "../lib/utils/getStatusIcon.js";
 
-let loggedIn = false
-let initPromise = null
+let loggedIn = false;
+let initPromise = null;
 
 export function initAternos() {
-  if (loggedIn) return initPromise
+  if (loggedIn) return initPromise;
 
   if (!initPromise) {
     initPromise = (async () => {
-      await loginAternos()
-      loggedIn = true
-      console.log("✅ Aternos ready")
-      startAternosPoller()
-    })()
+      await loginAternos();
+      loggedIn = true;
+      console.log("✅ Aternos ready");
+      startAternosPoller();
+    })();
   }
 
-  return initPromise
+  return initPromise;
 }
 
 /* =======================
    START SERVER
 ======================= */
 export async function aternosStart() {
-  await initAternos()
-  return await startServer()
+  await initAternos();
+  return await startServer();
 }
 
 /* =======================
@@ -34,20 +35,22 @@ export async function aternosStart() {
 ======================= */
 export async function aternosStatus() {
   try {
-    const data = await readServerState()
+    const data = await readServerState();
 
     const rawIp = data.ip || "";
 
     const match = rawIp.match(/([\w-]+\.aternos\.me)/);
     const cleanIp = match ? match[1].toLowerCase().trim() : "unknown";
 
+    const icon = getStatusIcon(data.status);
+
     return `
-      :satellite: Status: **${data.status == "online" ? `${data.status} 🟢` : `${data.status} 🔴`}**
+      :satellite: Status: **${data.status} ${icon}**
       :busts_in_silhouette: Players: **${data.players}**
       :globe_with_meridians: IP: **${cleanIp}**
       :watch: Updated: <t:${Math.floor(data.lastUpdate / 1000)}:R>
-    `
+    `;
   } catch {
-    return "⚠️ No data"
+    return "⚠️ No data";
   }
 }
